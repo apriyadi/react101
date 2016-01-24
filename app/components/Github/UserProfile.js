dfc07718d8acd4a04c524c1781cf63c41e58670d
Aditia Priyadi <aditia.priyadi@touchcast.co.nz>
2016-01-25 08:40:38 +1300

    chore(ES6): Rewrite some of the functions with es6

--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -1,15 +1,28 @@
-var React = require('react');
+import React from 'react'
 
-var UserProfiles = React.createClass({
-	render: function() {
-		return (
-			<div> 
-				<p>USER PROFILES </p>
-				<p>Username is: {this.props.username}</p>
-				<p>Bio is: {this.props.bio.name}</p>
-			</div>
-		)
-	}
-});
+class UserProfile extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Ueer profile</h1>
+        {this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
+        {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
+        {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
+        {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
+        {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
+        {this.props.bio.company && <li className="list-group-item">Company: {this.props.bio.company}</li>}
+        {this.props.bio.followers && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
+        {this.props.bio.following && <li className="list-group-item">Following: {this.props.bio.following}</li>}
+        {this.props.bio.following && <li className="list-group-item">Public Repos: {this.props.bio.public_repos}</li>}
+        {this.props.bio.blog && <li className="list-group-item">Blog: <a href={this.props.bio.blog}> {this.props.bio.blog}</a></li>}
+      </div>
+    )
+  }
+}
 
-module.exports = UserProfiles;
+UserProfile.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  bio: React.PropTypes.object.isRequired
+}
+
+export default UserProfile
